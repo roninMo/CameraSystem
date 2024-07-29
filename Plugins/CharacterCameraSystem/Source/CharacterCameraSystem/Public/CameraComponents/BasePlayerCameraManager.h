5a2df7ae383a2cc6eb565489c73fe187996e9501
Kieran Schwegman <kschwegman1@gmail.com>
2024-07-28 20:03:30 -0400

    Update BasePlayerCameraManager.h

--- Plugins/CharacterCameraSystem/Source/CharacterCameraSystem/Public/CameraComponents/BasePlayerCameraManager.h
+++ Plugins/CharacterCameraSystem/Source/CharacterCameraSystem/Public/CameraComponents/BasePlayerCameraManager.h
@@ -16,8 +16,27 @@
 
 
 class ACharacterCameraLogic;
+
+
 /**
+ * The Character Camera System's Camera Manager. Camera character's player controllers should use this camera manager class
+ *
+ * A PlayerCameraManager is responsible for managing the camera for a particular
+ * player. It defines the final view properties used by other systems (e.g. the renderer),
+ * meaning you can think of it as your virtual eyeball in the world. It can compute the 
+ * final camera properties directly, or it can arbitrate/blend between other objects or 
+ * actors that influence the camera (e.g. blending from one CameraActor to another).
  * 
+ * The PlayerCameraManagers primary external responsibility is to reliably respond to
+ * various Get*() functions, such as GetCameraViewPoint. Most everything else is
+ * implementation detail and overrideable by user projects.
+ * 
+ * By default, a PlayerCameraManager maintains a "view target", which is the primary actor
+ * the camera is associated with. It can also apply various "post" effects to the final 
+ * view state, such as camera animations, shakes, post-process effects or special 
+ * effects such as dirt on the lens.
+ *
+ * @see https://docs.unrealengine.com/latest/INT/Gameplay/Framework/Camera/
  */
 UCLASS()
 class CHARACTERCAMERASYSTEM_API ABasePlayerCameraManager : public APlayerCameraManager
